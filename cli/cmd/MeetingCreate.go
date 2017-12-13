a476f2224daf4b2787845e171809854293e49773
LeungChiHo <510769688@qq.com>
2017-12-13 22:26:29 +0800

    1

--- cli/cmd/MeetingCreate.go
+++ cli/cmd/MeetingCreate.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"net/http"
   "io/ioutil"
-  "github.com/LeungChiHo/Agenda-Service/service/entity"
+  entity "github.com/LeungChiHo/Agenda-Service/service/entity"
 	"github.com/spf13/cobra"
 )
 //-s 2000-01-01/00:00 -e 2001-01-01/00:00
